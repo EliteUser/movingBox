db1ca6cf3f46fe7a7036cb2dabb67db24e4f06ae
Andrey <unknownuser017@gmail.com>
2018-05-02 22:16:17 +0300

    modify Box

--- script.js
+++ script.js
@@ -2,53 +2,52 @@ function Box(x, y, color) {
   this.x = x;
   this.y = y;
   this.color = color;
-  this.drawBox();
+
+  this.drawBox = function () {
+    this.element = document.createElement("div");
+    this.element.classList.add("box");
+    this.element.style.left = this.x + "px";
+    this.element.style.top = this.y + "px";
+    this.element.style.backgroundColor = this.color;
+    document.querySelector("body").appendChild(this.element);
+  };
+
+  this.moveRight = function (distance) {
+    let dx = 10;
+    if (arguments.length > 0) {
+      dx = distance;
+    }
+    this.x += dx;
+    this.element.style.left = this.x + "px";
+  };
+
+  this.moveDown = function (distance) {
+    let dy = 10;
+    if (arguments.length > 0) {
+      dy = distance;
+    }
+    this.y += dy;
+    this.element.style.top = this.y + "px";
+  };
+
+  this.moveLeft = function (distance) {
+    let dx = 10;
+    if (arguments.length > 0) {
+      dx = distance;
+    }
+    this.x -= dx;
+    this.element.style.left = this.x + "px";
+  };
+
+  this.moveUp = function (distance) {
+    let dy = 10;
+    if (arguments.length > 0) {
+      dy = distance;
+    }
+    this.y -= dy;
+    this.element.style.top = this.y + "px";
+  };
 }
-
-Box.prototype.drawBox = function () {
-  this.element = document.createElement("div");
-  this.element.classList.add("box");
-  this.element.style.left = this.x + "px";
-  this.element.style.top = this.y + "px";
-  this.element.style.backgroundColor = this.color;
-  document.querySelector("body").appendChild(this.element);
-};
-
-Box.prototype.moveRight = function (distance) {
-  let dx = 10;
-  if (arguments.length > 0) {
-    dx = distance;
-  }
-  this.x += dx;
-  this.element.style.left = this.x + "px";
-};
-
-Box.prototype.moveDown = function (distance) {
-  let dy = 10;
-  if (arguments.length > 0) {
-    dy = distance;
-  }
-  this.y += dy;
-  this.element.style.top = this.y + "px";
-};
-
-Box.prototype.moveLeft = function (distance) {
-  let dx = 10;
-  if (arguments.length > 0) {
-    dx = distance;
-  }
-  this.x -= dx;
-  this.element.style.left = this.x + "px";
-};
-
-Box.prototype.moveUp = function (distance) {
-  let dy = 10;
-  if (arguments.length > 0) {
-    dy = distance;
-  }
-  this.y -= dy;
-  this.element.style.top = this.y + "px";
-};
 
 var box1 = new Box(0, 0, "green");
 
